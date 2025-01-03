8b6a137e3a48c8aeb58a617319e05e9c4ff6a1b7
jonathas.v.oliveira <jonathas.v.oliveira@gmail.com>
2025-01-03 11:04:32 -0300

    fix: change home message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p className="bg-red-500 p-2 m-2">Olá mundo!</p>
+      <p className="bg-primary p-2 m-2 rounded-lg">Home!</p>
     </div>
   );
 }
